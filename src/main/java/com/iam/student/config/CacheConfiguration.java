c3b50298e4ccf445191a0dde163121326a62fee3
xVivix <umad0000@gmail.com>
2019-04-24 16:28:50 +0200

    Last one

--- src/main/java/com/iam/student/config/CacheConfiguration.java
+++ src/main/java/com/iam/student/config/CacheConfiguration.java
@@ -38,6 +38,7 @@ public class CacheConfiguration {
             cm.createCache(com.iam.student.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.iam.student.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.iam.student.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.iam.student.domain.Student.class.getName(), jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
